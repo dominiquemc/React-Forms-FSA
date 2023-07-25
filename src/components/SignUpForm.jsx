dab80a76ed261081566425268a9e7e1c608740c6
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-07-25 00:54:21 -0500

    created SignUpForm component, and imported to App.jsx

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,3 +1,19 @@
+import { useState } from "react";
+
 export default function SignUpForm() {
-  return <h2>Sign Up</h2>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  return (
+    <>
+      <h2>Sign Up</h2>
+      <form>
+        <label>Username: </label>
+        <input type="username" />
+        <label> Password: </label>
+        <input type="password" />
+        <button>Submit</button>
+      </form>
+    </>
+  );
 }
