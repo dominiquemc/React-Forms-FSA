13899c988e96138a4160580b66877dc2efc15acb
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-07-26 15:35:09 -0500

    moved states to parent component, and passed in the states as props to children

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,33 @@ import Authenticate from "./components/Authenticate";
 import { useState } from "react";
 
 export default function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [token, setToken] = useState(null);
+  const [error, setError] = useState(null);
 
   return (
     <>
-      <SignUpForm token={token} setToken={setToken} />
-      <Authenticate token={token} setToken={setToken} />
+      <SignUpForm
+        token={token}
+        setToken={setToken}
+        error={error}
+        setError={setError}
+        username={username}
+        password={password}
+        setUsername={setUsername}
+        setPassword={setPassword}
+      />
+      <Authenticate
+        token={token}
+        setToken={setToken}
+        error={error}
+        setError={setError}
+        username={username}
+        password={password}
+        setUsername={setUsername}
+        setPassword={setPassword}
+      />
     </>
   );
 }
